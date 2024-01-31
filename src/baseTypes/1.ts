0f680d34445aa42fd4ea7f7216158a76dbc4c2b8
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2024-01-31 18:40:55 +0200

    add

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -5,6 +5,6 @@ let name: string = 'Max';
 let toggle: boolean = true;
 let empty: null = null;
 let notInitialize: undefined;
-let callback:(a:number)=> number = (a) => { return 100 + a };
+let callback:(a:number) => number = (a) => { return 100 + a };
 
 export {};
